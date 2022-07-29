ac0079b87de4d88809a3856820275cf816781216
unknown <corman.tanguy.ct@gmail.com>
2022-07-29 10:57:19 +0200

    finish drawing table

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # picture-tableau
+
+reference : [https://github.com/devkodeio/the-dom-challenge/blob/main/pixel-art/README.md](https://github.com/devkodeio/the-dom-challenge/blob/main/pixel-art/README.md)
--- js/script.js
+++ js/script.js
@@ -4,37 +4,104 @@
  * @param rows Number of rows
  * @param rows Number of cols
  */
-function PixelArt(el, rows, cols) {
-    // write logic to create pixel art grid.
-    let tabl = document.createElement("table");
 
-    for (let i = 0; i < rows; i++) {
+class Drawing 
+{
+    constructor() 
+    {
+        this.color = "white";
+        this.listColor = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "brown", "black", "white"];
+    }
+
+    /*
+        function
+            create grid
+        input:
+            el -> element parent
+            rows -> nombre de lignes
+            cols -> nombre de colonnes
+        output:
+            one
+    */
+    pixelArt(el, rows, cols) 
+    {
+        let tabl = document.createElement("table");
+    
+        //create coloring board
+        for (let i = 0; i < rows; i++) {
+            let row = document.createElement("tr");
+            for (let j = 0; j < cols; j++) {
+                let col = document.createElement("td");
+                col.className="drawing white";
+                row.appendChild(col);
+            }
+            tabl.appendChild(row);
+        }
         let row = document.createElement("tr");
-        for (let j = 0; j < cols; j++) {
+        let color = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "brown", "black", "white"];
+        
+        //create color palette
+        for (let j = 0; j < cols; j++) 
+        {
             let col = document.createElement("td");
-            col.className="white";
-            col.idName="drawing";
+            col.className="color "+color[j];
             row.appendChild(col);
         }
         tabl.appendChild(row);
+        
+        //add table to element
+        let element = document.getElementById(el);
+        element.appendChild(tabl);
     }
-    let row = document.createElement("tr");
-    let color = ["red", "green", "blue", "yellow", "orange", "purple", "pink", "brown", "black", "white"];
-    for (let j = 0; j < cols; j++) 
+
+    /*
+        function
+            change color
+        input:
+            el -> element
+        output:
+            none
+    */
+    pickColor(el) 
     {
-        let col = document.createElement("td");
-        col.className=color[j];
-        col.idName="color";
-        row.appendChild(col);
+        let color = el.className;
+        this.color = color.substring(6);
     }
-    tabl.appendChild(row);
-    
-    let element = document.getElementById(el);
-    element.appendChild(tabl);
+
+    /*
+        function
+            add event listener to cell
+        input:
+            none
+        output:
+            none
+    */
+    setEvent() 
+    {
+        //add event listener to cell drawing. update color
+        let drawings = document.getElementsByClassName("drawing");
+        for (let i = 0; i < drawings.length; i++) {
+            drawings[i].addEventListener("dragover", function(e) {
+                e.target.className = "drawing "+grid.color;
+            });
+            drawings[i].addEventListener("dragstart", function(e) {
+                e.target.className = "drawing "+grid.color;
+            });
+        }
+
+        //recover color to cell color and update Drawing.color
+        let colors = document.getElementsByClassName("color");
+        for (let i = 0; i < colors.length; i++) 
+        {
+            colors[i].addEventListener("click", function(e) 
+            {
+                grid.pickColor(e.target);
+            })
+        }
+    }
 }
 
-function changeColor(el) {
-    let color = el.className;
-    let drawing = document.getElementById("drawing");
-    drawing.className = color;
-}
+// main
+let grid = new Drawing ();
+grid.pixelArt("grid", 10, 10);
+grid.setEvent();
